161a4650253d9d9426caf6b5fdd39209ad1f0091
Thiago Luis <thiagoluisrpinho@hotmail.com>
2018-10-14 23:01:23 -0300

    Missed last exercise

--- Course 12 - Data Visualization with ggplot2(part 2)/chapter_05_exercise_09.r
+++ Course 12 - Data Visualization with ggplot2(part 2)/chapter_05_exercise_09.r
@@ -16,3 +16,25 @@
 # Try the function on other datasets we've worked with throughout this course:
 # mtcars dataset: am described by cyl
 # Vocab dataset: vocabulary described by education.
+
+# Load all packages
+library(ggplot2)
+library(reshape2)
+library(dplyr)
+library(ggthemes)
+
+# Script generalized into a function
+mosaicGG
+
+# BMI described by age (as previously seen)
+mosaicGG(adult, X = "SRAGE_P", FILL = "RBMI")
+
+# Poverty described by age
+mosaicGG(adult, X =  "SRAGE_P", FILL = "POVLL")
+
+# mtcars: am described by cyl
+mosaicGG(mtcars, "cyl", "am")
+
+# Vocab: vocabulary described by education
+library(carData)
+mosaicGG(Vocab, "education", "vocabulary")
